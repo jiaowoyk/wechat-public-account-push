0cb512564ddee2d18d56a3bcdc52722c12e90dbd
Alfredo Miller <83146568+jiaowoyk@users.noreply.github.com>
2024-09-03 21:07:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'yTQOA6fLjhnlXaBSmPt7eM7D5a-eU2UZF_rcDB_H0UE',
 
   CALLBACK_USERS: [
     {
